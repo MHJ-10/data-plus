ad737b63c184859c228b73362970af3a5f42225a
MHJ-10 <mhjafari.dev@gmail.com>
2026-05-10 16:53:57 +0330

    fix pages background

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,8 +27,24 @@ export default function RootLayout({
       className={`${harmattan.className} h-full antialiased`}
       suppressHydrationWarning
     >
-      <body className="flex min-h-full flex-col bg-background">
+      {/* <body className="bg-background flex min-h-full flex-col">
         <Provider>{children}</Provider>
+      </body> */}
+
+      <body className="bg-background relative flex min-h-full flex-col">
+        <div className="pointer-events-none fixed inset-0 z-5">
+          <div className="from-accent/10 via-background to-accent/10 absolute inset-0 bg-linear-to-r" />
+
+          <div className="absolute inset-0 bg-[linear-gradient(to_right,#64748b0d_1px,transparent_1px),linear-gradient(to_bottom,#64748b0d_1px,transparent_1px)] bg-size-[48px_48px]" />
+
+          <div className="bg-warning/5 absolute top-0 left-1/4 size-150 rounded-full blur-3xl" />
+          <div className="bg-accent/10 absolute top-1/4 right-1/4 size-175 rounded-full blur-3xl" />
+          <div className="bg-success/5 absolute bottom-1/4 left-1/3 size-125 rounded-full blur-3xl" />
+        </div>
+
+        <Provider>
+          <div className="relative z-10">{children}</div>
+        </Provider>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -14,33 +14,20 @@ import {
 
 export default function Home() {
   return (
-    <div className="bg-background relative min-h-screen">
-      <div className="pointer-events-none fixed inset-0 z-5">
-        <div className="from-accent/10 via-background to-accent/10 absolute inset-0 bg-linear-to-r" />
-
-        <div className="absolute inset-0 bg-[linear-gradient(to_right,#64748b0d_1px,transparent_1px),linear-gradient(to_bottom,#64748b0d_1px,transparent_1px)] bg-size-[48px_48px]" />
-
-        <div className="bg-warning/5 absolute top-0 left-1/4 size-150 rounded-full blur-3xl" />
-        <div className="bg-accent/10 absolute top-1/4 right-1/4 size-175 rounded-full blur-3xl" />
-        <div className="bg-success/5 absolute bottom-1/4 left-1/3 size-125 rounded-full blur-3xl" />
+    <>
+      <div className="container mx-auto mb-40 flex flex-col gap-40 px-4">
+        <Header />
+        <Hero />
+        <SampleResult />
+        <SeeInAction />
+        <HowItWorks />
+        <PowerfulFeatures />
+        <VisualizationTypes />
+        <UseCases />
       </div>
-
-      <div className="relative z-10">
-        <div className="container mx-auto mb-40 flex flex-col gap-40 px-4">
-          <Header />
-          <Hero />
-          <SampleResult />
-          <SeeInAction />
-          <HowItWorks />
-          <PowerfulFeatures />
-          <VisualizationTypes />
-          <UseCases />
-        </div>
-
-        <TechnicalHighlight />
-        <CTA />
-        <Footer />
-      </div>
-    </div>
+      <TechnicalHighlight />
+      <CTA />
+      <Footer />
+    </>
   );
 }
